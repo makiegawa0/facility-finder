2e3a55f5a516edbf068adffb148e16cb20e95645
Maki Egawa <egawamaki@gmail.com>
2024-05-30 12:11:00 +0900

    build: update the title of the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "React Google Maps",
+  title: "Facility Nearby",
   description: "Using visgl.github.io/react-google-maps",
 };
 
